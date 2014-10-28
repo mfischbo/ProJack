1f6561362b5bdc133c91a54c8a9fd74363eda78a
Markus Fischboeck <m.fischboeck@googlemail.com>
2014-10-28 18:05:10 +0100

    smaller crap

--- modules/security/security.js
+++ modules/security/security.js
@@ -107,9 +107,9 @@ ProJack.security.directive("userSelector", ['SecurityService', 'KT', function(se
         scope : {
             'selectAs': '=selectAs',
         },
-        template: '<div class="input-group">'
-            + '<select data-ng-model="selectAs" data-ng-options="u.id as u.login for u in users" class="form-control input-sm">'
-            + '<option value="" disabled>User</option>'
+        template: '<div class="form-group">'
+            + '<select data-ng-model="selectAs" data-ng-options="u.id as u.login for u in users" class="form-control">'
+            + '<option value="" disabled><b>User</b></option>'
             + '</select>'
             + '</div>',
         link:   function(scope, element, attrs) {
